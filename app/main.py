484723484e9bd818638b390e27915de4d4a79ebc
alberMontoya <alber172498@gmail.com>
2025-12-11 13:52:54 +0100

    psycopg changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+from random import randrange
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 app = FastAPI()
 
@@ -10,7 +14,18 @@ class Post(BaseModel):
 	title: str
 	content: str
 	published: bool = True
-	rating: Optional[int] = None
+
+try:
+	conn = psycopg2.connect(host='localhost', database='fastapi', user = 'postgres',
+						password='172498', client_encoding='utf8', 
+						cursor_factory=RealDictCursor)
+	cursor = conn.cursor()
+	print("Database connection OK")
+except Exception as error:
+	print("Connecting to database failed")
+	print(f"Error: {error}")
+	exit(1)
+		
 
 my_posts = [
 	{'title': 'First Post', 'content': 'This is the first post', 'id': 1},
@@ -36,23 +51,19 @@ async def index():
 
 @app.get('/posts')
 def get_posts():
-	return {"data": my_posts}
+	cursor.execute('''SELECT * FROM posts''')
+	posts = cursor.fetchall()
+	print(posts)
+	return {"data": posts}
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
-async def create_post(new_post: Post):
-	#pasa el objeto de Post a un diccionario
-	print(new_post.model_dump())
-	if new_post.model_dump().get('id') is None:
-		aux_post = new_post.model_dump()
-		aux_post['id'] = len(my_posts) + 1
-		my_posts.append(aux_post)
-	else:
-		my_posts.append(new_post.model_dump())
+def create_post(new_post: Post):
+	cursor.execute('''INSERT INTO posts (title, content, published) VALUES
+		(%s, %s, %s) RETURNING *''', (new_post.title, new_post.content, new_post.published))
+	inserted_post = cursor.fetchone()
+	conn.commit()
+	return {"data": inserted_post}
 	
-	return {
-		'message': 'Post created successfully',
-		'total posts': my_posts
-	}
 
 @app.get('/posts/latest')
 def get_latest_post():
@@ -62,31 +73,37 @@ def get_latest_post():
 
 @app.get('/posts/{id}')
 def get_post(id: int):
-	aux = find_post(id)
-	print(type(aux))
-	if aux == -1:
+	#id tiene que ser str para que no cree conflictos %s
+	cursor.execute('''SELECT * FROM posts WHERE id=%s''', (str(id)))
+	post = cursor.fetchone()
+	if not post:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item {id} not found")
-		
-	return aux
+	
+	return {"post_detail": post}
+
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-	aux_index = find_index_post(id)
-	if aux_index == -1:
+	cursor.execute(''' DELETE FROM posts WHERE id = %s RETURNING *''', (str(id)))
+	deleted_post = cursor.fetchone()
+	conn.commit()
+	if not deleted_post:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item {id} not found")
-	#my_posts.pop(aux_index)
-	del my_posts[aux_index]
+	
 	#no queremos devolver nada, para delete status 204
 	return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put('/posts/{id}')
 def update_post(id: int, post: Post):
-	print(post)
-	aux_index = find_index_post(id)
-	if aux_index == -1:
+	cursor.execute('''UPDATE posts SET title = %s,
+				content = %s,
+				published = %s
+				WHERE id = %s RETURNING *''', 
+				(post.title, post.content, post.published, str(id)))
+	updated_post = cursor.fetchone()
+	#commit siempre que hagamos cambios en bbdd (insert, delete, update)
+	conn.commit()
+	if not updated_post:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item {id} not found")
 	
-	post_dict = post.model_dump()
-	post_dict['id'] = id
-	my_posts[aux_index] = post_dict
-	return {"data": post_dict}
+	return {"data": updated_post}
