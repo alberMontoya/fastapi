ea6c56dfcb9070461a1cb1c917bd6e3e15625ab4
alberMontoya <alber172498@gmail.com>
2025-12-22 18:32:54 +0100

    minor fixes

--- app/oauth2.py
+++ app/oauth2.py
@@ -32,14 +32,14 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 def verify_access_token(token: str, credentials_exception):
 	try:
-		payload = jwt.decode(token, SECRET_KEY, algorithms=[ACCESS_TOKEN_EXPIRE_MINUTES])
+		payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 		id: str = payload.get("user_id")
 
 		if not id:
 			raise credentials_exception
 		
-		token_data = schemas.TokenData(id=id)
+		token_data = schemas.TokenData(id=str(id))
 	except PyJWTError:
 		raise credentials_exception
 	
