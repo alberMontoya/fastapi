fff32daf4c8952cffdb5a17b4b2d88c09f828bd2
alberMontoya <alber172498@gmail.com>
2026-01-24 16:30:39 +0100

    added lifespan to create tables

--- app/main.py
+++ app/main.py
@@ -6,18 +6,18 @@ from .database import engine
 from .routers import post, user, auth, vote
 
 #models.Base.metadata.create_all(bind=engine)
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-# 	async with engine.begin() as conn:
-# 		# run_sync permite ejecutar funciones sincrónicas (como create_all)
-# 		# dentro de una conexión asíncrona
-# 		await conn.run_sync(models.Base.metadata.create_all)
-# 		print(f"El driver activo es: {engine.dialect.driver}")
-# 	yield
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	async with engine.begin() as conn:
+		# run_sync permite ejecutar funciones sincrónicas (como create_all)
+		# dentro de una conexión asíncrona
+		await conn.run_sync(models.Base.metadata.create_all)
+		print(f"El driver activo es: {engine.dialect.driver}")
+	yield
+	print("Cerrando app")
 
 
-#app = FastAPI(lifespan=lifespan)
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get('/')
